7f2d8d1a64a38dd7ae70bc14c963f9d86bc27466
Devon <devon_3273@hotmail.com>
2021-10-27 21:10:44 -0600

    added button, doesnt work

--- js/client.js
+++ js/client.js
@@ -9,44 +9,44 @@ fetch('https://rickandmortyapi.com/api/character')
   .then(function(data){
     // Any code that depends on the `data` must go in this block
     const chars = document.querySelector('.chars');
-    const button = document.querySelector('.btn')
+    const button = document.querySelector('button');
+
     let id ;
-
-
-  function randomId(){
+    // New Character Generator
+    const newChar = function (){
       id = [Math.floor(Math.random() * `${data.results.length}`)];
-      console.log(id);
-      return [Number(id)];
+      return id;
     }
-    button.addEventListener("click", randomId);
-    
-    randomId();
-    
+    // Invoking Function
+    newChar();
+    // Button doesnt work
+    button.addEventListener(`click`, newChar);
+
     chars.innerHTML = `
     <figure>
-    <h2>${data.results[Number(id)].name}</h2>
-    <a href="${data.results[Number(id)].image}">
-    <img src="${data.results[Number(id)].image}" alt="Random Character from Rick and Morty">
+    <h2>${data.results[id].name}</h2>
+    <a href="${data.results[id].image}">
+    <img src="${data.results[id].image}" alt="Random Character from Rick and Morty">
     </a>
     <figcaption>
     <h3>Information</h3>
     <ul>
-    <li>Status: ${data.results[[Number(id)]].status}</li>
-    <li>Species: ${data.results[Number(id)].species}</li>
-    <li>Origin: ${data.results[Number(id)].origin.name}</li>
-    <li>Created: ${data.results[Number(id)].created}</li>
-    <li><a href="${data.results[Number(id)].url}">API Info</a></li>
+    <li>Status: ${data.results[id].status}</li>
+    <li>Species: ${data.results[id].species}</li>
+    <li>Origin: ${data.results[id].origin.name}</li>
+    <li>Created: ${data.results[id].created}</li>
+    <li><a href="${data.results[id].url}">API Info</a></li>
     </ul>
     </figcaption>
     </figure>
     <button>New Character</button>
     `;
-    console.log(data.results[Number(id)]);
-
+    
   })
+  
   .catch(function(err){
     // An error or `reject` from any of the above `.then()` blocks will end up here.
-    //alert('Something went wrong! Please refresh your page or come back later!')
+    alert('Something went wrong! Please refresh your page or come back later!')
     console.log(err);
   });
 
