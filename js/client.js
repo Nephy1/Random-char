5a3c1f3023968c1c3ab35dc39d9f7a03ebd779f3
Devon <devon_3273@hotmail.com>
2021-10-27 20:27:36 -0600

    fixed array issue and bugs

--- js/client.js
+++ js/client.js
@@ -9,36 +9,45 @@ fetch('https://rickandmortyapi.com/api/character')
   .then(function(data){
     // Any code that depends on the `data` must go in this block
     const chars = document.querySelector('.chars');
-    // TODO : Fix random array, not working
-    const randomId = function(){
-      //randomId.length = [Math.floor(Math.random() * randomId.length)];
-      min = Math.ceil(0);
-      max = Math.floor(671);
-      return Math.floor(Math.random() * (max - min + 1) + min);
-      
-      };
-    console.log(randomId.length);
+    const button = document.querySelector('.btn')
+    let id ;
+
+
+  function randomId(){
+      id = [Math.floor(Math.random() * `${data.results.length}`)];
+      console.log(id);
+      return [Number(id)];
+    }
+    button.addEventListener("click", randomId);
+    
+    randomId();
+    
     chars.innerHTML = `
     <figure>
-    <h2>${data.results[randomId.length].name}</h2>
-    <a href="${data.results[randomId.length].image}">
-    <img src="${data.results[randomId.length].image}" alt="Random Character from Rick and Morty">
+    <h2>${data.results[Number(id)].name}</h2>
+    <a href="${data.results[Number(id)].image}">
+    <img src="${data.results[Number(id)].image}" alt="Random Character from Rick and Morty">
     </a>
     <figcaption>
     <h3>Information</h3>
     <ul>
-    <li>Status: ${data.results[randomId.length].status}</li>
-    <li>Species: ${data.results[randomId.length].species}</li>
-    <li>Origin: ${data.results[randomId.length].origin.name}</li>
-    <li>API Info: <a href="${data.results[randomId.length].url}">Click Me</a></li>
+    <li>Status: ${data.results[[Number(id)]].status}</li>
+    <li>Species: ${data.results[Number(id)].species}</li>
+    <li>Origin: ${data.results[Number(id)].origin.name}</li>
+    <li>Created: ${data.results[Number(id)].created}</li>
+    <li><a href="${data.results[Number(id)].url}">API Info</a></li>
     </ul>
     </figcaption>
     </figure>
+    <button>New Character</button>
     `;
-    console.log(data);
+    console.log(data.results[Number(id)]);
+
   })
   .catch(function(err){
     // An error or `reject` from any of the above `.then()` blocks will end up here.
     //alert('Something went wrong! Please refresh your page or come back later!')
     console.log(err);
   });
+
+  
